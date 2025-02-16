c1f77198f228033af75c869b0fb1ce89b5ff9a87
Vahap Gözenelioğlu <gozeneliogluvahap@gmail.com>
2025-02-16 16:45:02 +0300

    IMemoryCache Get and Set values

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,14 +1,27 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers;
 
-public class ProductController
+public class ProductController : Controller
 {
     private readonly IMemoryCache _memoryCache;
     
     public ProductController(IMemoryCache memoryCache)
     {
         _memoryCache = memoryCache;
+    }
+
+    public IActionResult Index()
+    {
+        _memoryCache.Set<string>("time", DateTime.Now.ToString(CultureInfo.InvariantCulture));
+        return View();
+    }
+
+    public IActionResult Show()
+    {
+        ViewBag.time = _memoryCache.Get<string>("time");
+        return View();
     }
 }
